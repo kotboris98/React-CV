999610d4426ead26ac1efdcb3ca5721769c075c0
kotboris98 <borisvip169@gmail.com>
2025-02-05 02:23:17 +0300

    Initial commit

---
+++ react-CV/src/Header/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <div>
+            <header>
+                <a>Обо мне</a>
+                <a>Мой стек</a>
+                <a>Проекты</a>
+                <a>Контакты</a>
+            </header>
+        </div>
+    )
+}
+
+export default Header
