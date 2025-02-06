a6d4780eb0c4d24afcbb036a87d58171b81e520a
kotboris98 <borisvip169@gmail.com>
2025-02-07 01:23:13 +0300

    Initial commit

--- react-CV/src/App.jsx
+++ react-CV/src/App.jsx
@@ -1,42 +1,35 @@
 import { useState } from 'react'
 import './App.css'
-import '../src/components/Header/ModalWindow.css'
+import './components/Modal/Modal'
+import Modal from './components/Modal/Modal.jsx';
+import './components/Modal/Modal.css';
 
-function App() {
-  const [modal, setModal] = useState(false);
+const App = () => {
+  const [modalActive, setModalActive] = useState(true);
 
   return (
-    <header>
-    <div className="menu">
-        <a>Обо мне</a>
-        <a>Мой стек</a>
-        <a>Проекты</a>
-        <a>Контакты</a>
-    </div>
+    <><header>
+          <div className="menu">
+              <a>Обо мне</a>
+              <a>Мой стек</a>
+              <a>Проекты</a>
+              <a>Контакты</a>
+          </div>
 
-    <button onClick={() => setModal(!modal)} className="btn">Связаться</button>
+          <button className="btn">Связаться</button>
 
-    {!modal ? '' : (
-    <div className='modal'>
-        <div onClick={() => setModal(!modal)} className='close'>
-            <button>Закрыть</button>
-        </div>
-        <div className='content'>My modal</div>
-    </div>
-    )}
-
-    <div className="icons">
-        <a href="https://t.me/kotboris98">
-            <img src='../src/assets/telegram.png' className="icon"></img>
-        </a> 
-        <a href="https://instagram.com/kotboris98" className="icon">
-            <img src='../src/assets/instagram.png' className="icon"></img>
-        </a>
-        <a href="https://vk.com/kotboris_98" className="icon">
-            <img src='../src/assets/vk.png' className="icon"></img>
-        </a> 
-    </div>
-</header>
+          <div className="icons">
+              <a href="https://t.me/kotboris98">
+                  <img src='../src/assets/telegram.png' className="icon"></img>
+              </a>
+              <a href="https://instagram.com/kotboris98" className="icon">
+                  <img src='../src/assets/instagram.png' className="icon"></img>
+              </a>
+              <a href="https://vk.com/kotboris_98" className="icon">
+                  <img src='../src/assets/vk.png' className="icon"></img>
+              </a>
+          </div>
+      </header><Modal active={modalActive} setActive={setModalActive} /></>
 )
 }
 
