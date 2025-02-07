c500b66a23c5ebf9b9e35529bd1d1c3792f2e6f4
kotboris98 <borisvip169@gmail.com>
2025-02-07 17:55:44 +0300

    Initial commit

--- react-CV/src/App.jsx
+++ react-CV/src/App.jsx
@@ -3,9 +3,10 @@ import './App.css'
 import './components/Modal/Modal'
 import Modal from './components/Modal/Modal.jsx';
 import './components/Modal/Modal.css';
+import './components/Bio.css';
 
 const App = () => {
-  const [modalActive, setModalActive] = useState(true);
+  const [modalActive, setModalActive] = useState(false);
 
   return (
     <><header>
@@ -16,7 +17,7 @@ const App = () => {
               <a>Контакты</a>
           </div>
 
-          <button className="btn">Связаться</button>
+          <button className="btn" onClick={() => setModalActive(true)}>Связаться</button>
 
           <div className="icons">
               <a href="https://t.me/kotboris98">
@@ -29,7 +30,27 @@ const App = () => {
                   <img src='../src/assets/vk.png' className="icon"></img>
               </a>
           </div>
-      </header><Modal active={modalActive} setActive={setModalActive} /></>
+      </header>
+      <Modal active={modalActive} setActive={setModalActive} />
+      <main>
+        <div className='bio'>
+            <div className='bio__text'>
+                <div className='bio__first__heading'>
+                    <img src='../src/assets/programmer.svg' className='bio__icon__programmer'></img>
+                    <h2 className='bio__heading'>Обо мне</h2>
+                </div>
+                <ul>
+                    <li className='bio__li'>Начинающий Frontend-разработчик</li>
+                    <li className='bio__li'>Придерживаюсь чистого и аккуратного кода</li>
+                    <li className='bio__li'>Стремлюсь к саморазвитию, получению нового опыта и работе над настоящими проектами</li>
+                </ul>
+            </div>
+            <div className='photo'>
+                <img src='../src/assets/photo_of_me.jpg' className='photo__of__me'></img>
+            </div>
+        </div>
+      </main>
+      </>
 )
 }
 
--- react-CV/src/components/Modal/Modal.jsx
+++ react-CV/src/components/Modal/Modal.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import './Modal.css';
 
 const Modal = ({active, setActive}) => {
     return (
-        <div className="modal">
-            <div className="modal_content"></div>
+        <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
+            <div className="modal__content" onClick={e => e.stopPropagation()}>
+                <p className="modal__text">Связаться со мной можно по электронной почте:</p>
+                <h2>borisvip169@gmail.com</h2>
+            </div>
         </div>
     )
 }
