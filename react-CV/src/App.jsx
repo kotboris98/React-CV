c49c149551699ffd7a9796c308ef89f63910ee7d
kotboris98 <borisvip169@gmail.com>
2025-03-06 19:27:34 +0300

    Initial commit

--- react-CV/src/App.jsx
+++ react-CV/src/App.jsx
@@ -70,6 +70,7 @@ const App = () => {
               </div>
             </div>
           </div>)
+
           : ( 
             <div className='navigation'>
             <div className="menu">
@@ -94,10 +95,14 @@ const App = () => {
           </div>
           )}
       </header>
+
+
           <Modal active={modalActive} setActive={setModalActive} />
           <ModalMenu active={modalMenuActive} setActive={setModalMenuActive} />
-          <main>
-              <div className='bio'>
+
+
+          <main> {isPortrait ? (
+              <><div className='bio'>
                   <div className='bio__text'>
                       <div className='bio__article'>
                           <img src='../src/assets/programmer.svg' className='bio__icon'></img>
@@ -122,65 +127,148 @@ const App = () => {
                   <div className='photo'>
                       <img src='../src/assets/photo_of_me.jpg' className='photo__of__me'></img>
                   </div>
-              </div>
-              <div className='stack__and__projects'>
-                <div className='bio__article'>
-                  <img src='../src/assets/tools.svg' className='bio__icon'></img>
-                  <h2 className='bio__heading'>Мой стек</h2>
-                </div>
-              <div className='stack'>
-                  <div className='stack__icons'>
-                      <img src='../src/assets/html.svg' className='stack__icon'></img>
-                      <img src='../src/assets/css.svg' className='stack__icon'></img>
-                      <img src='../src/assets/js.svg' className='stack__icon'></img>
-                      <img src='../src/assets/ts.svg' className='stack__icon'></img>
-                      <img src='../src/assets/react.svg' className='stack__icon'></img>
-                      <img src='../src/assets/git.svg' className='stack__icon'></img>
-                  </div>
-              </div>
-              <div className='bio__article'>
-                  <img src='../src/assets/documents.svg' className='bio__icon'></img>
-                  <h2 className='bio__heading'>Мои проекты</h2>
-              </div>
-              <div className='projects'>
-                  <div className='project__block'>
-                      <img src='../src/assets/S1.png' className='projects__screenshot'></img>
-                      <div className='project__description'>
-                          <div>
-                              <div><h2 className='project__text'>Название:</h2><h1>Лендинг веб-страницы проекта Learn Up</h1></div>
-                              <div><h2 className='project__text'>Технологии:</h2><h1>HTML, CSS</h1></div>
+              </div><div className='stack__and__projects'>
+                      <div className='bio__article'>
+                          <img src='../src/assets/tools.svg' className='bio__icon'></img>
+                          <h2 className='bio__heading'>Мой стек</h2>
+                      </div>
+                      <div className='stack'>
+                          <div className='stack__icons'>
+                              <img src='../src/assets/html.svg' className='stack__icon'></img>
+                              <img src='../src/assets/css.svg' className='stack__icon'></img>
+                              <img src='../src/assets/js.svg' className='stack__icon'></img>
+                              <img src='../src/assets/ts.svg' className='stack__icon'></img>
+                              <img src='../src/assets/react.svg' className='stack__icon'></img>
+                              <img src='../src/assets/git.svg' className='stack__icon'></img>
                           </div>
                       </div>
-                  </div>
-                  <div className='project__block'>
-                      <img src='../src/assets/S2.png' className='projects__screenshot'></img>
-                      <div className='project__description'>
-                          <div>
-                              <div><h2 className='project__text'>Название:</h2><h1>Калькулятор</h1></div>
-                              <div><h2 className='project__text'>Технологии:</h2><h1>JavaScript</h1></div>
+                      <div className='bio__article'>
+                          <img src='../src/assets/documents.svg' className='bio__icon'></img>
+                          <h2 className='bio__heading'>Мои проекты</h2>
+                      </div>
+                      <div className='projects'>
+                          <div className='project__block'>
+                              <img src='../src/assets/S1.png' className='projects__screenshot'></img>
+                              <div className='project__description'>
+                                  <div>
+                                      <div><h2 className='project__text'>Название:</h2><h1>Лендинг веб-страницы проекта Learn Up</h1></div>
+                                      <div><h2 className='project__text'>Технологии:</h2><h1>HTML, CSS</h1></div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className='project__block'>
+                              <img src='../src/assets/S2.png' className='projects__screenshot'></img>
+                              <div className='project__description'>
+                                  <div>
+                                      <div><h2 className='project__text'>Название:</h2><h1>Калькулятор</h1></div>
+                                      <div><h2 className='project__text'>Технологии:</h2><h1>JavaScript</h1></div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className='project__block'>
+                              <img src='../src/assets/S3.png' className='projects__screenshot'></img>
+                              <div className='project__description'>
+                                  <div>
+                                      <div><h2 className='project__text'>Название:</h2><h1>Блокнот заметок</h1></div>
+                                      <div><h2 className='project__text'>Технологии:</h2><h1>Typescript, React</h1></div>
+                                  </div>
+                              </div>
                           </div>
                       </div>
-                  </div>
-                  <div className='project__block'>
-                      <img src='../src/assets/S3.png' className='projects__screenshot'></img>
-                      <div className='project__description'>
-                          <div>
-                              <div><h2 className='project__text'>Название:</h2><h1>Блокнот заметок</h1></div>
-                              <div><h2 className='project__text'>Технологии:</h2><h1>Typescript, React</h1></div>
-                          </div>
+                      <div className='bio__article'>
+                          <img src='../src/assets/Phone.svg' className='bio__icon'></img>
+                          <h2 className='bio__heading'>Контакты</h2>
                       </div>
-                  </div>
-              </div>
-              <div className='bio__article'>
-                  <img src='../src/assets/Phone.svg' className='bio__icon'></img>
-                  <h2 className='bio__heading'>Контакты</h2>
-              </div>
-              <ul>
-                <li className='bio__li'>Электронная почта: <h2>borisvip169@gmail.com</h2></li>
-                <li className='bio__li'>Гитхаб: <h2><a href='https://github.com/kotboris98'>https://github.com/kotboris98</a></h2></li>
-              </ul>
-            </div>
+                      <ul>
+                          <li className='bio__li'>Электронная почта: <h2>borisvip169@gmail.com</h2></li>
+                          <li className='bio__li'>Гитхаб: <h2><a href='https://github.com/kotboris98'>https://github.com/kotboris98</a></h2></li>
+                      </ul>
+                  </div></>) : 
+                  
+                  (
+                <><div className='bio'>
+                      <div className='bio__text-mobile'>
+                          <div className='bio__article-mobile'>
+                              <img src='../src/assets/programmer.svg' className='bio__icon'></img>
+                              <h2 className='bio__heading-mobile'>Обо мне</h2>
+                          </div>
+                          <img src='../src/assets/photo_of_me.jpg' className='photo__of__me-mobile'></img>
+                          <ul>
+                              <li className='bio__li'>Начинающий Frontend-разработчик</li>
+                              <li className='bio__li'>Придерживаюсь чистого и аккуратного кода</li>
+                              <li className='bio__li'>Стремлюсь к саморазвитию, получению нового опыта и работе над настоящими проектами</li>
+                          </ul>
+                          <div className='bio__article-mobile'>
+                              <img src='../src/assets/internship.svg' className='bio__icon'></img>
+                              <h2 className='bio__heading'>Чем я занимался на стажировке</h2>
+                          </div>
+                          <ul>
+                              <li className='bio__li'>Верстка лендингов (сайтов) на HTML и CSS</li>
+                              <li className='bio__li'>Разработка браузерной игры на JavaScript</li>
+                              <li className='bio__li'>Разработка мобильного приложения на Angular</li>
+                              <li className='bio__li'>Разработка онлайн-магазина на Angular</li>
+                          </ul>
+                      </div>
+                  </div><div className='stack__and__projects'>
+                          <div className='bio__article-mobile'>
+                              <img src='../src/assets/tools.svg' className='bio__icon'></img>
+                              <h2 className='bio__heading'>Мой стек</h2>
+                          </div>
+                          <div className='stack'>
+                              <div className='stack__icons'>
+                                  <img src='../src/assets/html.svg' className='stack__icon'></img>
+                                  <img src='../src/assets/css.svg' className='stack__icon'></img>
+                                  <img src='../src/assets/js.svg' className='stack__icon'></img>
+                                  <img src='../src/assets/ts.svg' className='stack__icon'></img>
+                                  <img src='../src/assets/react.svg' className='stack__icon'></img>
+                                  <img src='../src/assets/git.svg' className='stack__icon'></img>
+                              </div>
+                          </div>
+                          <div className='bio__article-mobile'>
+                              <img src='../src/assets/documents.svg' className='bio__icon'></img>
+                              <h2 className='bio__heading'>Мои проекты</h2>
+                          </div>
+                          <div className='projects'>
+                              <div className='project__block'>
+                                  <img src='../src/assets/S1.png' className='projects__screenshot'></img>
+                                  <div className='project__description'>
+                                      <div>
+                                          <div><h2 className='project__text'>Название:</h2><h1>Лендинг веб-страницы проекта Learn Up</h1></div>
+                                          <div><h2 className='project__text'>Технологии:</h2><h1>HTML, CSS</h1></div>
+                                      </div>
+                                  </div>
+                              </div>
+                              <div className='project__block'>
+                                  <img src='../src/assets/S2.png' className='projects__screenshot'></img>
+                                  <div className='project__description'>
+                                      <div>
+                                          <div><h2 className='project__text'>Название:</h2><h1>Калькулятор</h1></div>
+                                          <div><h2 className='project__text'>Технологии:</h2><h1>JavaScript</h1></div>
+                                      </div>
+                                  </div>
+                              </div>
+                              <div className='project__block'>
+                                  <img src='../src/assets/S3.png' className='projects__screenshot'></img>
+                                  <div className='project__description'>
+                                      <div>
+                                          <div><h2 className='project__text'>Название:</h2><h1>Блокнот заметок</h1></div>
+                                          <div><h2 className='project__text'>Технологии:</h2><h1>Typescript, React</h1></div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className='bio__article-mobile'>
+                              <img src='../src/assets/Phone.svg' className='bio__icon'></img>
+                              <h2 className='bio__heading'>Контакты</h2>
+                          </div>
+                          <ul>
+                              <li className='bio__li'>Электронная почта: <h2>borisvip169@gmail.com</h2></li>
+                              <li className='bio__li'>Гитхаб: <h2><a href='https://github.com/kotboris98'>https://github.com/kotboris98</a></h2></li>
+                          </ul>
+                      </div></>)}
           </main>
+
+
       </><footer>
               <p>Developed and designed by <span>Boris Tyukov</span></p>
               <p>2025</p>
